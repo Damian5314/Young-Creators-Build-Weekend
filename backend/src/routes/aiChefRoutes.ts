f88bc11e567b749ba730f924e45b7b99e5b7e93a
Motje3 <mohamaadflaha2014@gmail.com>
2025-11-29 16:53:36 +0100

    undo

--- backend/src/routes/aiChefRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { generateRecipes } from '../controllers/aiChefController';
-
-const router = Router();
-
-router.post('/generate', generateRecipes);
-
-export default router;
